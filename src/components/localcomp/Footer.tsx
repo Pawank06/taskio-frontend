42e312d997716e9ef4f6e81ce9a7e3d65b305870
PawanK06 <pawan06kumar2003@gmail.com>
2024-06-11 19:27:54 +0530

    add link to in footer

--- src/components/localcomp/Footer.tsx
+++ src/components/localcomp/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="w-full mx-auto lg:px-16">
             <div className="text-xs px-4 md:px-0 md:text-lg">
-                Task.io is Built by <Link className="underline font-medium">Pawan</Link>. hosted on <Link className="underline font-medium">Vercel</Link>. the source code is available on <Link className="underline font-medium">GitHub</Link>.
+                Task.io is Built by <Link to="#" className="underline font-medium">Pawan</Link>. hosted on <Link to="#" className="underline font-medium">Vercel</Link>. the source code is available on <Link to="#" className="underline font-medium">GitHub</Link>.
             </div>
         </footer>
     )
